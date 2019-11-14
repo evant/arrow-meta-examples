0c44852abb5a139a905a22f7e51d047cd0a9b3e4
Evan Tatarka <evan.tatarka@willowtreeapps.com>
2019-11-14 13:33:07 -0500

    Move sample code to test sources

--- hello-world/use-plugin/src/main/kotlin/io/arrowkt/example/HelloWorld.kt
+++
@@ -1,9 +0,0 @@
-package io.arrowkt.example
-
-//metadebug
-
-fun helloWorld(): Unit = TODO()
-
-fun main(args: Array<String>) {
-  helloWorld()
-}
---
+++ hello-world/use-plugin/src/test/kotlin/HelloWorld.kt
@@ -0,0 +1,14 @@
+package io.arrowkt.example
+
+import org.junit.Test
+
+//metadebug
+
+fun helloWorld(): Unit = TODO()
+
+class HelloWorld  {
+  @Test
+  fun testHelloWorld() {
+    helloWorld()
+  }
+}
